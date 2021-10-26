d16b01df59ff1448ebc100ec231229a379232202
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2021-10-26 12:39:45 +0300

    added screenshot

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+![Screen shot](/../<branch name>//screenShot.png?raw=true "Optional Title")
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,55 @@
 import { Component } from "react";
 
+
 class Main extends Component {
     state={
          count: 0,
-         ElementStyle1: {backgroundColor:'green'},
-         ElementStyle2:{backgroundColor:'blue'}
+         backgroundColor:'yellow',
+         
     }
+    
     
     increment= () =>{
         this.setState({count:this.state.count+1});
     }
+
     decrement = () =>{
+        if(this.state.count>0){
         this.setState({count:this.state.count-1});
     }
+}
+
+    incrementFive= () =>{
+        this.setState({count:this.state.count+5});
+    }
+    
+    decrementFive = ()=>{
+        if(this.state.count>4){
+            this.setState({count:this.state.count-5});
+        }
+    }
+    
+    
     resetcounter = () =>{
         this.setState({count:0})
     }
-    addClass = () =>{
-        let btn = document.getElementsByClassName('btnShow');
-        if(this.state.count/2 !==0 ){
-            btn.backgroundColor=this.state.ElementStyle1.backgroundColor;
-        }
-        else {
-            btn.backgroundColor=this.state.ElementStyle2.backgroundColor;
-        }
-    }
+    
         render (){
+            let circleClass = "";
+            this.state.count===0
+               ? (circleClass = "circle")
+               : this.state.count % 2 ===0
+               ? (circleClass = "even")
+               : (circleClass = "odd");
+           
       return(
       <main>
         <div>
-                <div><button onChange={this.addClass} className="btnShow">{this.state.count}</button></div>
+                <div><h1 className={circleClass} >{this.state.count}</h1></div>
                 <button  onClick={this.increment} className="counter">Add one</button>
                 <button  onClick={this.decrement} className="counter">Remove one</button>
+                <button  onClick={this.incrementFive} className="counter">Add five</button>
+                <button  onClick={this.decrementFive} className="counter">Remove five</button>
                 <button  onClick={this.resetcounter} className="counter">Reset</button>
           </div>
         </main>
